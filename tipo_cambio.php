f1ac7c4f9b29ee43b960c4752ef6fac2f1a1cbe6
Raul-OXRI <jose.botzoc.30@gmail.com>
2024-03-26 17:11:44 -0600

    Se realizo con exito la prueba y se modifico el codigo y se dio mas vista como la plantilla

--- tipo_cambio.php
+++ tipo_cambio.php
@@ -15,7 +15,25 @@ function mostrar_tipo_cambio() {
     $tipo_cambio = get_tipo_cambio();
 
     // Mostrar el tipo de cambio
-    echo '<p>El tipo de cambio USD/GTQ es: ' . $tipo_cambio . '</p>';
+    echo '
+    <div style="margin: 0; padding: 20px">
+        <div style="display: flex; align-items: center; justify-content: space-between; padding: 20px; width: 350px;">
+            <h2 style="font-size: 19px; margin: 0;">Tipo de cambio de Dolar</h2>
+            <div style="display: flex; flex-direction: column; margin-left: auto;">
+                <div style="display: flex; flex-direction: row; align-items: center; margin-bottom: 5px;">
+                    <span style="margin-right: 5px;">Compra:</span>
+                    <span style="font-weight: bold;">Q</span>
+                    <span style="font-weight: bold;">' . $tipo_cambio . '</span>
+                </div>
+                <div style="display: flex; flex-direction: row; align-items: center;">
+                    <span style="margin-right: 5px;">Venta:</span>
+                    <span style="font-weight: bold;">Q</span>
+                    <span style="font-weight: bold;"></span>
+                </div>
+            </div>
+        </div>
+    </div>
+    ';
 
 }
 
