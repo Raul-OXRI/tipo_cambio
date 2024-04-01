f1df6b3e89a73ef4d95233f3023ebdee035d5b07
Raul-OXRI <jose.botzoc.30@gmail.com>
2024-04-01 09:02:55 -0600

    se realizo cambios steticos para la pagina

--- includes/api.php
+++ includes/api.php
@@ -1,37 +1,22 @@
 <?php
-
 function get_tipo_cambio() {
-
-   // URL de la API
    $url = "https://api.cambio.today/v1/full/USD/json?key=48369|G1WJmhkY64bMsrZVBsKX";
-
-   // Intentar obtener el contenido de la API
    $json = file_get_contents($url);
-
-   // Verificar si la solicitud fue exitosa
    if ($json === false) {
-       // Manejar el error de la solicitud fallida
        return "Error al obtener el tipo de cambio";
    }
 
-   // Decodificar el JSON
    $datos = json_decode($json);
 
-   // Verificar si la decodificación fue exitosa
    if ($datos === null || !isset($datos->result) || !isset($datos->result->conversion[8]->rate)) {
-       // Manejar el error de datos no válidos
        return "Error en los datos recibidos";
    }
 
-   // Obtener el tipo de cambio sin redondear
    $tipo_cambio = $datos->result->conversion[8]->rate;
 
-   // Redondear el tipo de cambio a dos dígitos después del punto decimal
-   $tipo_cambio_redondeado = number_format($tipo_cambio, 4);
+   $tipo_cambio_redondeado = number_format($tipo_cambio, 2);
 
-   // Devolver el tipo de cambio redondeado
    return $tipo_cambio_redondeado;
-
 }
 
 ?>
--- tipo_cambio.php
+++ tipo_cambio.php
@@ -1,43 +1,37 @@
 <?php
-/*
-Plugin Name: tipo de cambio
-Plugin URI: http://solodata.es
-Description: Este es un plugin que ayuda a mostrar el tipo de cambio 
-Version: 0.0.1
-*/
-
-// Cargar la API
+/**
+ * Plugin Name: tipo de cambio 
+ * Plugin URI: https://github.com/Raul-OXRI/tipo_cambio 
+ * Description: Este es un plugin que ayuda a mostrar el tipo de cambio  
+ * Author: José Raúl Botzoc Mérida
+ * Version: 0.0.2 
+ */
 require_once( dirname( __FILE__ ) . '/includes/api.php' );
-// Función para mostrar el tipo de cambio
+
+
+
 function mostrar_tipo_cambio() {
-
-    // Obtener el tipo de cambio
     $tipo_cambio = get_tipo_cambio();
-
-    // Mostrar el tipo de cambio
     echo '
     <div style="margin: 0; padding: 20px">
-        <div style="display: flex; align-items: center; justify-content: space-between; padding: 20px; width: 350px;">
-            <h2 style="font-size: 19px; margin: 0;">Tipo de cambio de Dolar</h2>
+        <div style="display: flex; align-items: center; justify-content: space-between; padding: 20px; width: 245px;">
+            <h2 style="font-size: 16px; margin: 0; font-family: Montserrat, sans-serif;">Conversión GTQ a USD</h2>
             <div style="display: flex; flex-direction: column; margin-left: auto;">
                 <div style="display: flex; flex-direction: row; align-items: center; margin-bottom: 5px;">
-                    <span style="margin-right: 5px;">Compra:</span>
+                    <span style="margin-right: 5px;"></span>
                     <span style="font-weight: bold;">Q</span>
                     <span style="font-weight: bold;">' . $tipo_cambio . '</span>
                 </div>
                 <div style="display: flex; flex-direction: row; align-items: center;">
-                    <span style="margin-right: 5px;">Venta:</span>
-                    <span style="font-weight: bold;">Q</span>
+                    <span style="margin-right: 5px;"></span>
+                    <span style="font-weight: bold;">$ 1.00</span>
                     <span style="font-weight: bold;"></span>
                 </div>
             </div>
         </div>
     </div>
     ';
-
 }
 
-// Agregar el shortcode
 add_shortcode( 'tipo_cambio', 'mostrar_tipo_cambio' );
-
 ?>
